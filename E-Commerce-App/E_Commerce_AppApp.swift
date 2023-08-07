a7cc27cb4b8cc87388deb06136f2914241164616
Sevda Abbasi <sevdaabbasi37@gmail.com>
2023-08-07 14:16:34 +0300

    Initial Commit

---
+++ E-Commerce-App/E_Commerce_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_Commerce_AppApp.swift
+//  E-Commerce-App
+//
+//  Created by Sevda Abbasi on 7.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct E_Commerce_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
