ff88eddf2a7ea6444f08c2539e089b4824d72f8e
Sevda Abbasi <sevdaabbasi37@gmail.com>
2023-08-07 23:52:18 +0300

    first commit

--- E-Commerce-App/ContentView.swift
+++ E-Commerce-App/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var currentTab: Tab = .Home
+    
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
+    
+    @Namespace var animation
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView (selection: $currentTab){
+           
+            Text("Home View")
+            Text("Search View")
+            Text("Notification View")
+            Text("Cart View")
+            Text("Profile View")
         }
         .padding()
     }
@@ -23,4 +33,31 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+enum Tab: String, CaseIterable{
+    case Home = "house"
+    case Search = "magnifyingglass.circile"
+    case Notification = "bell"
+    case Cart = "bag"
+    case Profile = "Person"
+    
+    var Tabname: String {
+        switch self {
+        case.Home:
+            return "Home"
+        case.Search:
+            return "Search"
+        case.Notification:
+            return "Notification"
+        case.Cart:
+            return "Cart"
+        case.Profile:
+            return "Profile"
+        }
+    }
+    
+}
+extension View {
+   func getSafeArea () -> UIEdgeInsets
 }
