b0ba93bf52cff1a3caf3785dd223b7f97ed84db8
Sevda Abbasi <sevdaabbasi37@gmail.com>
2023-08-10 01:47:32 +0300

    Designing and animating bottom navigation bar

--- E-Commerce-App/ContentView.swift
+++ E-Commerce-App/ContentView.swift
@@ -18,16 +18,83 @@ struct ContentView: View {
     
     var body: some View {
         TabView (selection: $currentTab){
-           
+            
             Text("Home View")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color("kPrimary"))
+                .tag(Tab.Home)
             Text("Search View")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color(red: 24, green: 0, blue: 0))
+                .tag(Tab.Search)
             Text("Notification View")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background()
+                .tag(Tab.Notification)
             Text("Cart View")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background()
+                .tag(Tab.Cart)
             Text("Profile View")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background()
+                .tag(Tab.Profile)
         }
-        .padding()
+        
+        .overlay(
+            HStack(spacing:0){
+                ForEach(Tab.allCases, id: \.rawValue){ tab in
+                    TabButton(tab: tab)
+                }
+                .padding(.vertical)
+                .padding(.bottom, getSafeArea().bottom == 0 ? 5 : (getSafeArea().bottom - 15))
+                .background(Color("kSecondary"))
+            }
+            ,
+            alignment: .bottom
+        )
+        .ignoresSafeArea(.all, edges: .bottom)
+        
+        
     }
-}
+func TabButton(tab: Tab) -> some View {
+    GeometryReader  {
+        proxy in
+        Button(action: {
+            withAnimation(.spring()){
+                currentTab = tab
+            }
+            
+        }, label: {
+            VStack(spacing: 0){
+                Image(systemName: currentTab == tab ? tab.rawValue + ".fill" : tab.rawValue)
+                    .resizable()
+                    .foregroundColor(Color("kPrimary"))
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 25, height: 25)
+                    .frame(maxWidth: .infinity)
+                    .background(
+                        ZStack{
+                            if currentTab == tab {
+                                MaterialEffect(style: .light)
+                                    .clipShape(Circle())
+                                    .matchedGeometryEffect(id: "Tab", in: animation)
+                                
+                                Text(tab.Tabname)
+                                    .foregroundColor(.primary)
+                                    .font(.footnote)
+                                    .padding(.top, 50)
+                            }
+                        }
+                    ).contentShape(Rectangle())
+                    .offset(y: currentTab == tab ? -15 : 0)
+            }
+        })
+    }
+    .frame(height: 25)
+    
+ }
+ }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -35,12 +102,13 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+
 enum Tab: String, CaseIterable{
     case Home = "house"
-    case Search = "magnifyingglass.circile"
+    case Search = "magnifyingglass.circle"
     case Notification = "bell"
     case Cart = "bag"
-    case Profile = "Person"
+    case Profile = "person"
     
     var Tabname: String {
         switch self {
@@ -58,6 +126,36 @@ enum Tab: String, CaseIterable{
     }
     
 }
+
 extension View {
-   func getSafeArea () -> UIEdgeInsets
+    func getSafeArea () -> UIEdgeInsets{
+        guard let screen = UIApplication.shared.connectedScenes.first as?
+                UIWindowScene
+        else {
+            return .zero
+        }
+        guard let safeArea = screen.windows.first?.safeAreaInsets
+                
+        else{
+            return .zero
+        }
+        return safeArea
+    }
 }
+
+struct MaterialEffect: UIViewRepresentable {
+    var style: UIBlurEffect.Style
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style:style))
+        
+        return view
+    }
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+    
+    }
+}
+
+
+
+
